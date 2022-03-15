b1bdcc949c4a9e39f0ba54c23e6a4eda496a94d7
akmahamud <am3153636@gmail.com>
2022-03-15 23:21:44 +0600

    my first app react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Friend name = "Akash Mahamud" phone = "01758819483"></Friend>
+    <Friend name = "Sagor Mahamud" phone = "01979480309"></Friend>
+    <Friend name = "jubeyr Ahamed" phone = "01904897570"></Friend>
+    
+    <Team name = "Readul Islam" aim = "MERN Stack Developer"></Team>
+    <Team name = "Nahid" aim = "Developer"></Team>
+    <Team name = "Jubyer Ahamed" aim = "MERN Stack Developer"></Team>
+    
+    
     </div>
   );
-}
+};
+
+function Friend (props) {
+  
+ return(
+   <div className="player">
+     <h1>{props.name}</h1>
+     <h4>{props.phone}</h4>
+   </div>
+ )
+};
+
+function Team (props){
+  console.log(props);
+  return(
+    <div className="teamMember">
+      <h1>{props.name}</h1>
+      <h4>{props.aim}</h4>
+    </div>
+  )
+};
 
 export default App;
